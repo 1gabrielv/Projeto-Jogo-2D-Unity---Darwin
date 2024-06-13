d66a1b43d1432049490c43c9a0600141b07dfe9d
João Silva <gabrielvsc@id.uff.br>
2024-06-13 14:01:14 -0300

    fase 2 quase concluida

--- Darwin (proprototipo)/Assets/zCoisas faseX/Scripts/Player.cs
+++ Darwin (proprototipo)/Assets/zCoisas faseX/Scripts/Player.cs
@@ -4,49 +4,61 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rbplayer;
+    public Rigidbody2D rbplayer;
     public float speed;
     public float jumpforce;
     private SpriteRenderer sr;
     public bool Infloor = true;
-    // Start is called before the first frame update
+    private Animator animator;
+    private bool isDead = false;  // Variável para controlar o estado de morte
+    GameObject cameraPos;
+
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         rbplayer = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        cameraPos = GameObject.Find("Main Camera");
+
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Jump();
+        if (!isDead)
+        {
+            Jump();
+            AtualizarAnimacoes();
+            cameraPos.transform.position = new Vector3(Mathf.Clamp(transform.position.x, 29.5f, 149.5f), Mathf.Clamp(transform.position.y, -4, 13.7f), cameraPos.transform.position.z);
+        }
     }
 
     private void FixedUpdate()
     {
-        MovePlayer();
+        if (!isDead)
+        {
+            MovePlayer();
+        }
     }
 
     void MovePlayer()
     {
         float HM = Input.GetAxisRaw("Horizontal");
-
         rbplayer.velocity = new Vector2(HM * speed, rbplayer.velocity.y);
 
-        if(HM > 0)
+        if (HM > 0)
         {
             sr.flipX = false;
         }
-        else if(HM < 0)
+        else if (HM < 0)
         {
             sr.flipX = true;
         }
-
     }
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump") && Infloor)
+        if (Input.GetButtonDown("Jump") && Infloor)
         {
             rbplayer.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
             Infloor = false;
@@ -54,10 +66,41 @@ public class Player : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.name == "Ground" || collision.gameObject.name == "esquerda" || collision.gameObject.name == "direita")
     {
-        if(collision.gameObject.name == "Ground")
+        Infloor = true;
+        jumpforce = 6.5f;  // Certifique-se de definir o valor correto para cada caso
+    }
+    if(collision.gameObject.name == "Wall")
+    {
+        Infloor = true;
+        jumpforce = 7.7f;
+    }
+    
+    if (collision.gameObject.CompareTag("Armadilhas"))
+    {
+        if (!isDead)  // Verifica se o personagem já não está morto
         {
-            Infloor = true;
+            isDead = true;  // Define o estado de morte
+            animator.SetBool("taPulando", false);
+            animator.SetTrigger("Dead");
+            rbplayer.velocity = Vector2.zero;  // Para o movimento do personagem
+            rbplayer.isKinematic = true;  // Torna o Rigidbody cinemático para impedir mais movimentação
         }
     }
 }
+
+    void AtualizarAnimacoes()
+    {
+        if (!isDead)  // Atualiza animações apenas se não estiver morto
+        {
+            animator.SetBool("taPulando", !Infloor);
+
+            float HM = Input.GetAxisRaw("Horizontal");
+            animator.SetBool("taCorrendo", HM != 0);
+        }
+    }
+    
+
+    }
---
+++ Darwin (proprototipo)/Assets/zCoisas faseX/Scripts/Spikes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    GameObject parede;
+    GameObject pB;
+    public bool encostou = false;
+    public float speed = 0.15f; // Velocidade de movimento ajustável
+
+    // Start é chamado antes do primeiro quadro de atualização
+    void Start()
+    {
+        parede = GameObject.Find("parede assustadora");
+        pB = GameObject.Find("ponto B");
+         // Verifique o nome correto no Unity
+    }
+
+    // Update é chamado uma vez por quadro
+    void Update()
+    {
+        // Verifica se o jogador encostou no ponto boneco 
+                // Move o GameObject atual em direção ao ponto B
+                transform.position = Vector3.MoveTowards(transform.position, pB.transform.position, speed * Time.deltaTime);
+
+    }
+}
